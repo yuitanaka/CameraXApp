13b7037d89e7c1eac76322f2da0d4993b4d18036
yui_tanaka <tanakayui.work@gmail.com>
2022-03-04 16:52:35 +0900

    add Function of Scan Barcode

--- app/src/main/java/com/example/cameraxapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraxapp/MainActivity.kt
@@ -16,13 +16,25 @@ import java.util.concurrent.Executor
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.pm.PackageManager
 import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
 import java.util.*
 import android.provider.MediaStore
 import android.content.ContentValues
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import android.util.Log
+import android.util.SparseIntArray
+import android.view.Surface
+import androidx.annotation.RequiresApi
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
 import java.lang.Exception
 
 typealias LumaListener = (luma: Double) -> Unit
@@ -37,6 +49,16 @@ class MainActivity : AppCompatActivity() {
     private var recording: Recording? = null
 
     private lateinit var cameraExecuter: ExecutorService
+
+    // （バーコード、QRコード解析番外）
+//    private val ORIENTATIONS = SparseIntArray()
+//
+//    init {
+//        ORIENTATIONS.append(Surface.ROTATION_0, 0)
+//        ORIENTATIONS.append(Surface.ROTATION_90, 90)
+//        ORIENTATIONS.append(Surface.ROTATION_180, 180)
+//        ORIENTATIONS.append(Surface.ROTATION_270, 270)
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -194,6 +216,13 @@ class MainActivity : AppCompatActivity() {
 //                })
 //            }
 
+            // バーコード
+            val barcodeAnalyzer = ImageAnalysis.Builder().build().also {
+                it.setAnalyzer(cameraExecuter, BarcodeImageAnalyzer{
+
+                })
+            }
+
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
@@ -204,7 +233,7 @@ class MainActivity : AppCompatActivity() {
                 // Bind use cases to camera
                 // ここで紐付けしたものだけアプリで使用できる
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture, videoCapture
+                    this, cameraSelector, preview, imageCapture, videoCapture, barcodeAnalyzer
                 )
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -235,9 +264,38 @@ class MainActivity : AppCompatActivity() {
                 finish()
             }
         }
+
+        // （バーコード、QRコード解析番外）CameraXライブラリを使用しない場合、デバイスの回転角度とデバイス内のカメラセンサーの
+        // 向きから画像を計算する。Android5未満の場合など。
+        /**
+         * Get the angle by which an image must be rotated given the device's current
+         * orientation.
+         */
+//        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+//        @Throws(CameraAccessException::class)
+//        fun getRotationCompensation(cameraId: String, activity: Activity, isFrontFacing: Boolean): Int {
+//            // Get the device's current rotation relative to its "native" orientation.
+//            // Then, from the ORIENTATIONS table, look up the angle the image must be
+//            // rotated to compensate for the device's rotation.
+//            val deviceRotation = activity.windowManager.defaultDisplay.rotation
+//            var rotationCompensation = ORIENTATIONS.get(deviceRotation)
+//
+//            // Get the device's sensor orientation.
+//            val cameraManager = activity.getSystemService(CAMERA_SERVICE) as CameraManager
+//            val sensorOrientation = cameraManager
+//                .getCameraCharacteristics(cameraId)
+//                .get(CameraCharacteristics.SENSOR_ORIENTATION)!!
+//
+//            if (isFrontFacing) {
+//                rotationCompensation = (sensorOrientation + rotationCompensation) % 360
+//            } else { // back-facing
+//                rotationCompensation = (sensorOrientation - rotationCompensation + 360) % 360
+//            }
+//            return rotationCompensation
+//        }
     }
 
-    // 画像解析用クラス
+    // 画像解析（光度）用クラス
     private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
 
         private fun ByteBuffer.toByteArray(): ByteArray {
@@ -256,6 +314,70 @@ class MainActivity : AppCompatActivity() {
             listener(luma)
 
             image.close()
+        }
+    }
+
+    // 画像解析（バーコード、QRコード
+    // デバイスのカメラから画像をキャプチャする場合など、InputImageオブジェクトからmedia.Imageオブジェクトを
+    // 作成するには、オブジェクトと画像の回転をInputImage.fromMediaImage()に渡す
+    // CameraXライブラリを使用する場合、OnImageCapturedListener/ImageAnalysis.Analyzerクラスは回転値を計算する
+    private class BarcodeImageAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
+        @SuppressLint("UnsafeOptInUsageError")
+        override fun analyze(imageProxy: ImageProxy) {
+            val mediaImage = imageProxy.image
+            if (mediaImage != null) {
+                val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+                // Pass image to an ML Kit Vision API
+                scanBarcode(image)
+
+                imageProxy.close()
+            }
+        }
+
+        private fun scanBarcode(image: InputImage) {
+            // Aztecコード（バーコード形式の一種）を検出する
+            val options = BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(
+                    Barcode.FORMAT_AZTEC
+                )
+                .build()
+
+            val scanner = BarcodeScanning.getClient()
+
+            val result = scanner.process(image)
+                .addOnSuccessListener { barcodes ->
+                    // バーコード認識操作成功
+                    // barcodes: 画像で検出されたバーコード。バーコードごとに
+                    // 　　　　　　入力画像の境界座標とバーコードによってエンコード
+                    //           された生データを取得可能
+                    for (barcode in barcodes) {
+                        val bounds = barcode.boundingBox
+                        val corner = barcode.cornerPoints
+
+                        val rawValues = barcode.rawValue
+
+                        val valueType = barcode.valueType
+                        // See API reference for complete list of supported types
+                        when (valueType) {
+                            Barcode.TYPE_WIFI -> {
+                                val ssid = barcode.wifi!!.ssid
+                                val password = barcode.wifi!!.password
+                                val type = barcode.wifi!!.encryptionType
+
+                                Log.d(TAG, "ssid: ${ssid}")
+                            }
+                            Barcode.TYPE_URL -> {
+                                val title = barcode.url!!.title
+                                val url = barcode.url!!.url
+
+                                Log.d(TAG, "ssid: ${title}")
+                            }
+                        }
+                    }
+                }
+                .addOnFailureListener {
+
+                }
         }
     }
 
